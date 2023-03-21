a123d201666d58f1546d5283c77e5c072209cd4d
Bijay Nayak <bijay6779@gmail.com>
2023-03-21 15:09:44 +0530

    refactor

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
 from routers.routes import router as coderouter
-app = FastAPI()
+
+app = FastAPI(title="AuthCheck API",
+              docs_url="/",
+              )
 app.include_router(coderouter)
