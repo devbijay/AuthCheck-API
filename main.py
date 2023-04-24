d85ac232f3c2c5d421b5cb3d4192712e4c6f9d52
Bijay Nayak <bijay6779@gmail.com>
2023-04-24 11:40:13 +0530

    Route Change

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from routers.routes import router as coderouter
 
-app = FastAPI(title="AuthCheck API",
+app = FastAPI(title="VerifiedProof API",
               docs_url="/",
               )
 app.include_router(coderouter)
--- routers/routes.py
+++ routers/routes.py
@@ -1,57 +1,65 @@
-from typing import Optional
+import os
+from typing import Optional, Union
 from fastapi import APIRouter, Header, Security, Depends, HTTPException, status, Query
 from fastapi.security import APIKeyHeader
 from dotenv import load_dotenv
-
-from models.models import CodeStatus, CodeDetails, BuyerData, UserDetails
+from redis import Redis
+from models.models import CodeStatus, CodeDetails, BuyerData, UserDetails, InvalidCode
 from services.database import db
+
 
 load_dotenv()
 router = APIRouter()
 
+
+
 api_key_header = APIKeyHeader(name="X-API-Key")
+
+redis = Redis(
+    host=os.getenv('REDIS_HOST'),
+    port=int(os.getenv('REDIS_PORT')),
+    password=os.getenv('REDIS_PASS')
+)
 
 
 async def auth_required(api_key: Optional[str] = Depends(api_key_header)):
     if not api_key:
         raise HTTPException(status_code=401, detail="API key is missing in header")
 
-    if user := db.users.find_one({"api": api_key}, {"username": 1}):
-        return user.get("username")
+    if user := redis.get(api_key):
+        return user.decode('utf-8')
 
     raise HTTPException(status_code=401, detail="Invalid API key")
 
 
-@router.get("/status", response_model=CodeStatus, tags=["Code Management"])
-async def get_code_status(code: str = Query(description="Code Id or Qr Id", min_length=8),
-                          user: str = Depends(auth_required)):
-    if result := db.codes.find_one({"owner_name": user, "$or": [{"code": code}, {"_id": code}]},
-                                   {"_id": 0, "status": 1}):
-        return CodeStatus(code=code, status="used" if result.get("status") else "not_used")
-
-    return CodeStatus(code=code, status="Invalid Code")
-
-
-@router.post("/status", response_model=CodeDetails, tags=["Code Management"])
+@router.post("/status", response_model=CodeDetails, responses={404: {"model": InvalidCode}}, tags=["Code Management"])
 async def get_code_details(code: str = Query(description="Code Id or Qr Id", min_length=8),
                            user: str = Depends(auth_required)):
-    if result := db.codes.find_one({"owner_name": user, "$or": [{"code": code}, {"_id": code}]}):
-        buyer_data = BuyerData(name=result.get("buyer_name"),
-                               phone=result.get("buyer_phone"),
-                               email=result.get("buyer_email"),
-                               purchase_source=result.get("purchase_source"),
-                               verified_on_utc=str(result.get("verified_on"))
-                               )
+
+    code_filter = {'_id': code} if len(code) == 26 else {'code': code, 'owner_name': user}
+
+    if result := db.unused_codes.find_one(code_filter):
+        return CodeDetails(code=code,
+                           qr_id=result.get("_id"),
+                           status="not_used",
+                           buyer_data=""
+                           )
+
+    elif result := db.codes.find_one(code_filter):
         return CodeDetails(code=code,
                            qr_id=result.get("_id"),
                            status="used" if result.get("status") else "not_used",
-                           buyer_data=buyer_data
-                           )
+                           buyer_data=BuyerData(name=result.get("buyer_name"),
+                                                phone=result.get("buyer_phone"),
+                                                email=result.get("buyer_email"),
+                                                buyer_city=result.get("b_city"),
+                                                purchase_source=result.get("purchase_source"),
+                                                verified_on_utc=str(result.get("verified_on"))))
 
-    return CodeStatus(code=code, status="Invalid Code")
+    return InvalidCode(code=code, status="Invalid Code")
 
 
-@router.get("/userinfo", response_model=UserDetails, tags=["User Management"])
+@router.post("/userinfo", response_model=UserDetails, tags=["User Management"])
 async def get_account_information(user: str = Depends(auth_required)):
     q_res = db.users.find_one({"username": user}, {"role": 0, "activity": 0, "signup_date": 0, "password": 0})
     return UserDetails(
@@ -62,14 +70,3 @@ async def get_account_information(user: str = Depends(auth_required)):
         subdomain=q_res['settings'].get('subdomain')
     )
 
-
-@router.post("/userinfo", response_model=UserDetails, tags=["User Management"])
-async def change_password(user: str = Depends(auth_required)):
-    q_res = db.users.find_one({"username": user}, {"role": 0, "activity": 0, "signup_date": 0, "password": 0})
-    return UserDetails(
-        username=q_res['username'],
-        name=q_res['name'],
-        email=q_res['email'],
-        credit_count=q_res['credit'],
-        subdomain=q_res['settings'].get('subdomain')
-    )
